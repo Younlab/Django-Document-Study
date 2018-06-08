614e48b0cae712351a6a0bb7753e5811aa393edf
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-08 16:46:13 +0900

    2018.06.08 저장

--- app/config/settings.py
+++ app/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'models.fields.apps.FieldsConfig',
+    'models.forignkey.apps.ForignkeyConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
