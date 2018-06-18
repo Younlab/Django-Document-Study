bd851889809551b53c5cf2d4225694cfff569ef1
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-18 11:43:15 +0900

    ForeignKey

--- app/models/forignkey/models.py
+++ app/models/forignkey/models.py
@@ -1,14 +1,8 @@
 from django.db import models
 
-class Manufacturer(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
 class Car(models.Model):
     manufacturer = models.ForeignKey(
-        Manufacturer,
+        'Manufacturer',
         on_delete=models.CASCADE,
     )
 
@@ -16,3 +10,19 @@ class Car(models.Model):
 
     def __str__(self):
         return self.name
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    instructor = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        related_name='students',
+        blank=True,
+        null=True,
+    )
