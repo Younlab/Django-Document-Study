2c71f674164dc3d6c7a9b559ecda7950e8501e97
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-19 12:16:06 +0900

    ManyToMany self symmetrical=False

--- app/models/many_to_many/models/self_symmetrical_false_intermediate.py
+++ app/models/many_to_many/models/self_symmetrical_false_intermediate.py
@@ -33,43 +33,26 @@ class TwitterUser(models.Model):
     def __str__(self):
         return self.name
 
-# class Relation(models.Model):
-#     """
-#     TwitterUser 간의 MTM 관계를 정의
-#         from_user
-#         to_user
-#         follow인지, block 인지를 판단.
-#     """
-#     CHOICES_RELATION_TYPE = (
-#         ('f', 'Follow'),
-#         ('b', 'Block'),
-#     )
-#     from_user = models.ForeignKey(
-#         TwitterUser,
-#         on_delete=models.CASCADE,
-#         related_name='relations_by_from_user',
-#
-#     )
-#
-#     to_user = models.ForeignKey(
-#         TwitterUser,
-#         on_delete=models.CASCADE,
-#         related_name='relations_by_to_user',
-#     )
-#     # 입력값을 제한하는 choices옵션 추가
-#     relation_type = models.CharField(
-#         max_length=1,
-#         choices=CHOICES_RELATION_TYPE,
-#     )
-#     # 관계의 생성일 기록
-#     created_at = models.DateTimeField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return 'from({}), to({}), {}'.format(
-#             self.from_user.name,
-#             self.to_user.name,
-#             self.get_relation_type_display(),
-#         )
+    @property
+    def following_relations(self):
+        # 내가 follow 하고 있는 relation 들을 리턴
+        # TwitterUser.objects.get(name=self.name).filter(Relation.objects.get())
+        # Relation.objects.filter(from_user=self)
+        # Relation.objects.filter(from_user=self, relation_type='f')
+        return self.relations_by_from_user.filter(relation_type='f')
+
+    @property
+    def follower_relations(self):
+        # 나를 follow 하고 있는 Relation 들을 리턴
+        # (내가 Relation의 to_user이며, relation_type 이 'f' 인 경우)
+        return self.relations_by_to_user.filter(relation_type='f')
+
+    @property
+    def block_relations(self):
+        # 내가 block 하고 있는 relation 들을 리턴
+        # Relation.objects.filter(from_user=self, relation_type='f')
+        return self.relations_by_from_user.filter(relation_type='b')
+
 
 class Relation(models.Model):
     CHOICES_RELATION_TYPE = (
